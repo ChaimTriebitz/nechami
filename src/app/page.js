bca5a52cd23ddb5beb76b9577641fb1ab55e8146
chaim <chaim.triebitz@bhbsdventures.com>
2025-03-23 18:54:07 +0200

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,123 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   return (
+      <div className='page'>
+         <header >
+            <h1 >
+               הצטרפו לנבחרת
+            </h1>
+            <h2 >
+               אנחנו פותחים את דלתנו ומצרפים עורכי דין עם שאיפות להתמקצע בתביעות רשלנות רפואית
+            </h2>
+         </header>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+         <main >
+            <section className='card'>
+               <h2>מנחה הקורס</h2>
+               <div className='details' >
+                  <div className="img">
+                     <Image
+                        src="/Nechami.jpg"
+                        alt="עו״ד נחמי וולק"
+                        width={250}
+                        height={250}
+                        priority
+                     />
+                  </div>
+                  <div className='content'>
+                     <h3 >עו״ד נחמי מייזליש וולק</h3>
+                     <h4 >שותפה וראשת מחלקת דיני נזיקין במשרד</h4>
+                     <h5>הדד, רוט, שנהר ושות׳ - משרד עורכי דין</h5>
+                     <a
+                        href="https://har.law/har_lawyers/%d7%a0%d7%97%d7%9e%d7%99-%d7%9e%d7%99%d7%99%d7%96%d7%9c%d7%99%d7%a9-%d7%95%d7%95%d7%9c%d7%a7/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                     >
+                        למידע נוסף
+                     </a>
+                  </div>
+               </div>
+            </section>
+
+            <section >
+               <h2>לוח זמנים</h2>
+               <p >חמישה מפגשים בימי שלישי, 17:30-20:15</p>
+               <div >
+                  <div >
+                     מפגש ראשון
+                     <span>תאריך יפורסם בהמשך</span>
+                  </div>
+                  <div className="meeting-card">
+                     מפגש שני
+                     <span>תאריך יפורסם בהמשך</span>
+                  </div>
+                  <div className="meeting-card">
+                     מפגש שלישי
+                     <span>תאריך יפורסם בהמשך</span>
+                  </div>
+                  <div className="meeting-card">
+                     מפגש רביעי
+                     <span>תאריך יפורסם בהמשך</span>
+                  </div>
+                  <div className="meeting-card">
+                     מפגש חמישי
+                     <span>תאריך יפורסם בהמשך</span>
+                  </div>
+               </div>
+            </section>
+
+            <section >
+               <h2>עלות הקורס</h2>
+               <div >
+                  <div >
+                     <h3>מחיר רגיל</h3>
+                     <p>1,900 ₪</p>
+                     <span>+ מע״מ</span>
+                  </div>
+                  <div >
+                     <div >הרשמה מוקדמת</div>
+                     <h3>הרשמה מוקדמת</h3>
+                     <p>1,450 ₪</p>
+                     <span>+ מע״מ</span>
+                  </div>
+               </div>
+               <p >התשלום אפשרי רק באמצעות העברה בנקאית למשרד</p>
+            </section>
+
+            <section >
+               <h2>תנאי קבלה</h2>
+               <p>
+                  הקורס מיועד לעורכי דין חדשים (עד 5 שנים) המעוניינים להתמחות בתחום הרשלנות הרפואית.
+               </p>
+            </section>
+
+            <section>
+               <h2>מיקום</h2>
+               <p>
+                  המפגשים יתקיימו במשרדנו ברחוב דוד המלך 2, תל אביב, קומה 22
+               </p>
+            </section>
+
+            <section >
+               <h2>מדיניות הקורס</h2>
+               <ul>
+                  <li>הקורס יכיל עד 12 משתתפים</li>
+                  <li>מי שלא יוכל להגיע למפגש יהיה זכאי להשתתף באותו מפגש בקורס הבא</li>
+                  <li>ניתן לבטל הרשמה עד חודש לפני תחילת הקורס</li>
+                  <li>ההשתתפות מותנית בהסכמת המשרד ועשויה להתבטל בכל שלב</li>
+                  <li>הקבלה מותנית במענה על שאלון וראיון עם עו״ד נחמי</li>
+               </ul>
+            </section>
+
+            <section>
+               <h2>פרטיות</h2>
+               <p>המשרד מתחייב לא לסחור במידע האישי של הנרשמים.</p>
+            </section>
+         </main>
+
+         <footer>
+         </footer>
+      </div>
+   );
 }
